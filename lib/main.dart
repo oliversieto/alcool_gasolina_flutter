5aa10ae874676dfe03b461e52fae54ea02b05d25
Renato Oliveira <oliversieto@gmail.com>
2021-03-04 22:07:31 -0400

    refactor: criado o input referente ao alcool

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:alcool_gasolina/widgets/logo.widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_masked_text/flutter_masked_text.dart';
 
 void main() {
   runApp(MyApp());
@@ -21,6 +22,7 @@ class MyApp extends StatelessWidget {
 }
 
 class HomePage extends StatelessWidget {
+  var _gasCtrl = new MoneyMaskedTextController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,6 +30,38 @@ class HomePage extends StatelessWidget {
       body: ListView(
         children: <Widget>[
           Logo(),
+          Row(
+            children: [
+              Container(
+                width: 100,
+                alignment: Alignment.centerRight,
+                child: Text(
+                  'Álcool',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 35,
+                      fontFamily: 'Big Shoulders Display'),
+                ),
+              ),
+              SizedBox(
+                width: 20,
+              ),
+              Expanded(
+                child: TextFormField(
+                  controller: _gasCtrl,
+                  keyboardType: TextInputType.number,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 45,
+                    fontFamily: 'Big Shoulders Display',
+                  ),
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                  ),
+                ),
+              )
+            ],
+          )
         ],
       ),
     );

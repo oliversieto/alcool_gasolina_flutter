188a5c050f91f53f9d7eeaf58fa957cda26c0438
Renato Oliveira <oliversieto@gmail.com>
2021-03-06 16:00:26 -0400

    reafcator: melhorado a estrutura da home.
    criado o método de cálculo e reset

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,11 @@
-import 'dart:ffi';
-
-import 'package:alcool_gasolina/widgets/input.widget.dart';
-import 'package:alcool_gasolina/widgets/loading-button.widget.dart';
-import 'package:alcool_gasolina/widgets/logo.widget.dart';
-import 'package:alcool_gasolina/widgets/submit-form.widget.dart';
-import 'package:alcool_gasolina/widgets/success.widget.dart';
+import 'package:alcool_gasolina/pages/home.page.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_masked_text/flutter_masked_text.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(App());
 }
 
-class MyApp extends StatelessWidget {
+class App extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -23,29 +16,6 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.deepPurple,
       ),
       home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  MoneyMaskedTextController _gasCtrl = new MoneyMaskedTextController();
-  MoneyMaskedTextController _alcCtrl = new MoneyMaskedTextController();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Theme.of(context).primaryColor,
-      body: ListView(
-        children: <Widget>[
-          Logo(),
-          SubmitForm(
-            gasCtrl: _gasCtrl,
-            alcCtrl: _alcCtrl,
-            busy: false,
-            submitFunction: () {},
-          ),
-        ],
-      ),
     );
   }
 }
---
+++ lib/pages/home.page.dart
@@ -0,0 +1,82 @@
+import 'package:alcool_gasolina/widgets/logo.widget.dart';
+import 'package:alcool_gasolina/widgets/submit-form.widget.dart';
+import 'package:alcool_gasolina/widgets/success.widget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_masked_text/flutter_masked_text.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  MoneyMaskedTextController _gasCtrl = new MoneyMaskedTextController();
+
+  MoneyMaskedTextController _alcCtrl = new MoneyMaskedTextController();
+
+  bool _busy = false;
+
+  bool _completed = false;
+
+  String _resultText = 'Compensa utilizar álcool';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: ListView(
+        children: <Widget>[
+          Logo(),
+          _completed
+              ? Success(
+                  result: _resultText,
+                  reset: reset,
+                )
+              : SubmitForm(
+                  gasCtrl: _gasCtrl,
+                  alcCtrl: _alcCtrl,
+                  busy: _busy,
+                  submitFunction: calculate,
+                ),
+        ],
+      ),
+    );
+  }
+
+  void reset() {
+    setState(() {
+      _gasCtrl = new MoneyMaskedTextController();
+      _alcCtrl = new MoneyMaskedTextController();
+      _busy = false;
+      _completed = false;
+    });
+  }
+
+  Future calculate() {
+    double alc =
+        double.parse(_alcCtrl.text.replaceAll(new RegExp(r'[,.]'), '')) / 100;
+
+    double gas =
+        double.parse(_gasCtrl.text.replaceAll(new RegExp(r'[,.]'), '')) / 100;
+
+    double res = alc / gas;
+
+    setState(() {
+      _completed = false;
+      _busy = true;
+    });
+
+    return new Future.delayed(const Duration(seconds: 1), () {
+      setState(() {
+        if (res >= 0.7) {
+          _resultText = 'Compensa utilizar Gasolina!';
+        } else {
+          _resultText = 'Compensa utilizar Álcool!';
+        }
+
+        _busy = false;
+        _completed = true;
+      });
+    });
+  }
+}
